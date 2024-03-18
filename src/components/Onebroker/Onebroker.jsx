85cec02a3267c902778df09db87b18bb2650bc78
Huda-Roushdy <HodaRoshdy.Geeeklabs@gmail.com>
2024-03-18 13:59:44 -0700

    working on top-broker page

--- src/components/Onebroker/Onebroker.jsx
+++ src/components/Onebroker/Onebroker.jsx
@@ -1,8 +1,11 @@
 const OneBroker = ({num,word})=>{
     return(
-        <div>
-            <span>{num}</span>
+        <div className=" flex py-2 justify-between pe-4 ps-12 test bg-black">
+
+            <h5>{num}</h5>
             <h5>{word}</h5>
+
+   
 
         </div>
     )
