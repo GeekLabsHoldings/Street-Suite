b432ec39200da85dc693699344af45cb29195fa9
Xcoder2000 <ahmed.aly@geeklabs.agency>
2024-03-19 09:15:09 +0200

    folders arrangement

--- src/App.js
+++ src/App.js
@@ -2,30 +2,30 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 // import all sections as components from components folder
-import Footer from './components/home-page-sections/Footer/Footer';
-import HomePage from './pages/home-page/HomePage';
-import QuezzesPage from './pages/Quizzes/quezzes-page/QuezzesPage';
-import LeaderBoardPage from './pages/leaderboard/LeaderBoardBage';
-import FaqPage from './pages/FAQ-page/FaqPage';
-import PricingPage from './pages/pricing-page/PricingPage';
-import FeaturesPage from './pages/features-page/FeaturesPage';
-import QuizPage from './pages/Quizzes/quiz-page/QuizPage';
-import QuizzesFiltersPage from './pages/Quizzes/quizzesFilters-page/QuizzesFiltersPage';
-import QuizResultsPage from './pages/Quizzes/quizResults-page/QuizResultsPage';
-import CheckOutPage from './pages/pricing-page/checkout-page/CheckOutPage';
-import CompleteCheckout from './pages/pricing-page/checkout-page/complete-checkout-page/CompleteCheckout';
-import APIPage from './pages/API-page/APIPage';
-import AboutUsPage from './pages/AboutUs-page/AboutUsPage';
-import SignupPage from './pages/signup/SignupPage';
-import ContactPage from './pages/ContactPage/ContactPage';
-import ChangeLogs from './pages/ChangeLog-page/changeLog';
+import Footer from './website/components/home-page-sections/Footer/Footer';
+import HomePage from './website/pages/home-page/HomePage';
+import QuezzesPage from './website/pages/Quizzes/quezzes-page/QuezzesPage';
+import LeaderBoardPage from './website/pages/leaderboard/LeaderBoardBage';
+import FaqPage from './website/pages/FAQ-page/FaqPage';
+import PricingPage from './website/pages/pricing-page/PricingPage';
+import FeaturesPage from './website/pages/features-page/FeaturesPage';
+import QuizPage from './website/pages/Quizzes/quiz-page/QuizPage';
+import QuizzesFiltersPage from './website/pages/Quizzes/quizzesFilters-page/QuizzesFiltersPage';
+import QuizResultsPage from './website/pages/Quizzes/quizResults-page/QuizResultsPage';
+import CheckOutPage from './website/pages/pricing-page/checkout-page/CheckOutPage';
+import CompleteCheckout from './website/pages/pricing-page/checkout-page/complete-checkout-page/CompleteCheckout';
+import APIPage from './website/pages/API-page/APIPage';
+import AboutUsPage from './website/pages/AboutUs-page/AboutUsPage';
+import SignupPage from './website/pages/signup/SignupPage';
+import ContactPage from './website/pages/ContactPage/ContactPage';
+import ChangeLogs from './website/pages/ChangeLog-page/changeLog';
 import DashboardLayout from './dashboard/layouts/DashboardLayout';
-import WebsiteLayout from './layouts/WebsiteLayout';
+import WebsiteLayout from './website/layouts/WebsiteLayout';
 import AlertsPage from './dashboard/pages/alerts page/alertsPage';
 import TrainingPage from './dashboard/pages/training page/TrainingPage';
 import TradingFlowPage from './dashboard/pages/tradingFlow page/TradingFlowPage';
 
-import TopBroker from './pages/TopBrokerPage/TopBroker';
+import TopBroker from './website/pages/TopBrokerPage/TopBroker';
 
 
 function App() {
