f1daafdb1fb15212ef299f9ac0a5e5e3498930f6
Huda-Roushdy <HodaRoshdy.Geeeklabs@gmail.com>
2024-04-02 14:05:44 -0700

    top-brokers changes

--- src/website/pages/TopBrokerPage/TopBroker.jsx
+++ src/website/pages/TopBrokerPage/TopBroker.jsx
@@ -2,8 +2,6 @@ import { Button } from 'react-bootstrap';
 import OneBroker from '../../components/Onebroker/Onebroker';
 import PickCard from '../../components/PickCard/PickCard';
 import { useState } from 'react';
-// import { useNavigate } from "react-router-dom";
-import TestImg from '../../assets/broker-img1.png';
 import { useSelector, useDispatch } from 'react-redux'
 import { reset,removeItem } from '../../../redux/cardsSlice';
 // our data here
@@ -169,7 +167,7 @@ return(
                                     <div className="w-1/12 ">
                                     <svg 
                                     onClick={()=>{
-                                        removeItem(oneCard);
+                                        dispatch(removeItem(oneCard));
                                 }} 
                                     xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
