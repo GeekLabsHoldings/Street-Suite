47a58c40da154059716634fbcbf5cd8aabdccd2b
Xcoder2000 <ahmed.aly@geeklabs.agency>
2024-04-15 10:47:37 +0200

    end responsive

--- src/dashboard/pages/training page/SuiteAcademy/SuiteAcademy.jsx
+++ src/dashboard/pages/training page/SuiteAcademy/SuiteAcademy.jsx
@@ -4,18 +4,13 @@ import SearchBarStyles from "../../../components/searchBar/SearchBar.module.css"
 import searchIcon from "../../../assets/search.svg";
 import styles from "./SuiteAcademy.module.css";
 
-import notifIcon from "../../../assets/notif.svg";
 import { Link, NavLink } from 'react-router-dom';
 
 import advancedBadge from "../../../assets/advanced padge.png"
+import NotificationWindow from '../../../UI-components/NotificationWindow/NotificationWindow';
 const SuiteAcademy = () => {
 
     const [isNavOpen, setIsNavOpen] = useState(false);
-    const [openNotificationWindow, setOpenNotificationWindow] = useState(false);
-
-    const toggleNotificationWindow = () => {
-        setOpenNotificationWindow(!openNotificationWindow)
-    }
 
 
     // ST Suite academy page 
@@ -37,88 +32,7 @@ const SuiteAcademy = () => {
                                 </li>
                             </ul>
                         </div>
-                        <button className={SearchBarStyles.notificationBtn} type="button" onClick={toggleNotificationWindow}>
-                            <img src={notifIcon} alt="" />
-                            <span>6</span>
-                            <div className={openNotificationWindow ? SearchBarStyles.notification_window + " notification_window " + SearchBarStyles.open : SearchBarStyles.notification_window + " notification_window "} >
-                                <ul class="tabs-nav flex items-center">
-
-                                    {/* taps that show the appropriate content in leaderBoard page */}
-                                    <li><a href="#Alerts">Alerts</a></li>
-                                    <li><a href="#News">News</a></li>
-
-                                </ul>
-                                <div class={SearchBarStyles.tabs_content + " tabs-stage"}>
-
-                                    {/* Biggest Winners content that show when user click on Biggest Winners tab in leaderBoard page */}
-                                    <div id="Alerts" className='tab space-y-4'>
-                                        <div className={SearchBarStyles.notification_item}>
-                                            <p><span>$TSLA</span> just announced an acquisition of <span>$NFLX</span> at <span>$200 B</span>. </p>
-                                            <span>26 mins ago</span>
-                                        </div>
-
-                                        <div className={SearchBarStyles.notification_item}>
-                                            <p><span>$TSLA</span> just announced an acquisition of <span>$NFLX</span> at <span>$200 B</span>. </p>
-                                            <span>26 mins ago</span>
-                                        </div>
-
-                                        <div className={SearchBarStyles.notification_item}>
-                                            <p><span>$TSLA</span> just announced an acquisition of <span>$NFLX</span> at <span>$200 B</span>. </p>
-                                            <span>26 mins ago</span>
-                                        </div>
-
-                                        <div className={SearchBarStyles.notification_item}>
-                                            <p><span>$TSLA</span> just announced an acquisition of <span>$NFLX</span> at <span>$200 B</span>. </p>
-                                            <span>26 mins ago</span>
-                                        </div>
-
-                                        <div className={SearchBarStyles.notification_item}>
-                                            <p><span>$TSLA</span> just announced an acquisition of <span>$NFLX</span> at <span>$200 B</span>. </p>
-                                            <span>26 mins ago</span>
-                                        </div>
-
-                                        <div className={SearchBarStyles.notification_item}>
-                                            <p><span>$TSLA</span> just announced an acquisition of <span>$NFLX</span> at <span>$200 B</span>. </p>
-                                            <span>26 mins ago</span>
-                                        </div>
-
-                                    </div>
-
-                                    <div id="News" className='tab space-y-4'>
-                                        <div className={SearchBarStyles.notification_item}>
-                                            <p><span>$TSLA</span> just announced an acquisition of <span>$NFLX</span> at <span>$200 B</span>. </p>
-                                            <span>26 mins ago</span>
-                                        </div>
-
-                                        <div className={SearchBarStyles.notification_item}>
-                                            <p><span>$TSLA</span> just announced an acquisition of <span>$NFLX</span> at <span>$200 B</span>. </p>
-                                            <span>26 mins ago</span>
-                                        </div>
-
-                                        <div className={SearchBarStyles.notification_item}>
-                                            <p><span>$TSLA</span> just announced an acquisition of <span>$NFLX</span> at <span>$200 B</span>. </p>
-                                            <span>26 mins ago</span>
-                                        </div>
-
-                                        <div className={SearchBarStyles.notification_item}>
-                                            <p><span>$TSLA</span> just announced an acquisition of <span>$NFLX</span> at <span>$200 B</span>. </p>
-                                            <span>26 mins ago</span>
-                                        </div>
-
-                                        <div className={SearchBarStyles.notification_item}>
-                                            <p><span>$TSLA</span> just announced an acquisition of <span>$NFLX</span> at <span>$200 B</span>. </p>
-                                            <span>26 mins ago</span>
-                                        </div>
-
-                                        <div className={SearchBarStyles.notification_item}>
-                                            <p><span>$TSLA</span> just announced an acquisition of <span>$NFLX</span> at <span>$200 B</span>. </p>
-                                            <span>26 mins ago</span>
-                                        </div>
-                                    </div>
-
-                                </div>
-                            </div>
-                        </button>
+                        < NotificationWindow />
                     </div>
                     <div className={` ${SearchBarStyles.bars} flex md:hidden ${isNavOpen ? SearchBarStyles.open : ""}`} onClick={() => { setIsNavOpen(!isNavOpen) }}>
                         <div className={SearchBarStyles.bar}></div>
