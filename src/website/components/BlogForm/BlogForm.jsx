220be7f77bcd3ea460866888656c61d694c7c520
azgeeklabs <abdulrahmanzakigeeklabs@gmail.com>
2024-06-03 11:25:39 +0300

    commit

--- src/website/components/BlogForm/BlogForm.jsx
+++ src/website/components/BlogForm/BlogForm.jsx
@@ -1,28 +1,25 @@
-import './BlogForm.css';
+import "./BlogForm.css";
 
-const BlogForm = ({classname})=>{
-    return(
-// repeated part for every old blog
-             <div className={`flex md:gap-4 sm:gap-2 md:py-5 sm:py-3 bottomBorder ${classname}`}>
-                <div className='md:w-1/2 sm:w-2/5'>
-                <div className='divForImgSm md:h-28 sm:h-20'>
-                    
-                </div>
-                </div>
-{/* blog name and date of publish  */}
-                <div className="md:w-1/2 sm:w-3/5 ">
-                <div className="similarBlogs flex flex-col md:gap-3 sm:gap-2">
-                <h5>China Kicks U.S Butt In The Latest Whatever</h5>
-                <div className="bluecolor">
-                    <h6>25 MINS AGO</h6>
-                </div>
-                </div>
-                </div>
+const BlogForm = ({ classname }) => {
+  return (
+    // repeated part for every old blog
+    <div
+      className={`flex justify-between items-center md:gap-4 sm:gap-2 md:py-5 sm:py-3 bottomBorder ${classname}`}
+    >
+      <div className="md:w-1/2 sm:w-2/5 flex-grow">
+        <div className="divForImgSm md:h-28 sm:h-20 w-full"></div>
+      </div>
+      {/* blog name and date of publish  */}
+      <div className="md:w-1/2 sm:w-3/5 ">
+        <div className="similarBlogs flex flex-col md:gap-3 sm:gap-2">
+          <h5>China Kicks U.S Butt In The Latest Whatever</h5>
+          <div className="bluecolor">
+            <h6>25 MINS AGO</h6>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-                </div>
-
-   
-    )
-}
-
-export default BlogForm
+export default BlogForm;
