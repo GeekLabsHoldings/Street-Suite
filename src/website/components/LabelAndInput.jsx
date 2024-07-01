1cea7947cec663d54eb9b4ffeb0008b3ad0ef7d3
mohamedFathygeeklabs <mohamedfathygeeklabs@gmail.com>
2024-07-01 15:50:01 +0300

    Add conformation text

--- src/website/components/LabelAndInput.jsx
+++ src/website/components/LabelAndInput.jsx
@@ -1,24 +1,33 @@
-import Input from '@mui/joy/Input';
-import FormLabel from '@mui/joy/FormLabel';
+import Input from "@mui/joy/Input";
+import FormLabel from "@mui/joy/FormLabel";
 
-const LabelAndInput = ({labeltext,required,placeholder})=>{
-    
-    return(
-        // simple label and input section
-        <div className='labelAndInputStyle'>
-        <FormLabel required={required} className='mb-1 labelfont'
+const LabelAndInput = ({
+  labeltext,
+  required,
+  placeholder,
+  onChangeHandler,
+}) => {
+  return (
+    // simple label and input section
+    <div className="labelAndInputStyle">
+      <FormLabel
+        required={required}
+        className="mb-1 labelfont"
         sx={{
-        "& .MuiFormLabel-asterisk":{
-        color:'white'
-        }
-        }}>{labeltext}</FormLabel>
-        <Input
+          "& .MuiFormLabel-asterisk": {
+            color: "white",
+          },
+        }}
+      >
+        {labeltext}
+      </FormLabel>
+      <Input
         name={labeltext}
         type="text"
-        placeholder = {placeholder}
-        />
-        </div>
-    
-    )
-}
+        placeholder={placeholder}
+        onChange={onChangeHandler}
+      />
+    </div>
+  );
+};
 export default LabelAndInput;
--- src/website/pages/Positions-page/PositionsPage.jsx
+++ src/website/pages/Positions-page/PositionsPage.jsx
@@ -14,6 +14,13 @@ const PositionsPage = () => {
   const [index, setIndex] = useState(0);
   const [vacanciesList, setVacanciesList] = useState([]);
 
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [portfolioLink, setPortfolioLink] = useState("");
+  const [gitHubLink, setGitHubLink] = useState("");
+  const [cv, setCv] = useState("");
+
   async function getAllVacancies() {
     try {
       // window.scrollTo(0, 0);
@@ -60,151 +67,179 @@ const PositionsPage = () => {
           sx={{ transition: "2s" }}
         >
           {/* first position and it's expanded by default */}
-          
-          {vacanciesList.map((e,i)=>(
-            <div key={i} className="grayPositionBorder carrerPositionCollapse md:py-0 sm:py-4">
-            <Accordion
-              defaultExpanded={i == 0 ? true : false}
-              sx={{
-                "&.Mui-expanded": {
-                  border: "1px solid #53ACFF",
-                  borderRadius: "0.5rem",
-                },
-                "&.Mui-expanded p": {
-                  display: "none",
-                },
-                "&.Mui-expanded h2": {
-                  color: "#53ACFF",
-                },
-                padding: "0 2rem",
-              }}
-            //   expanded={i === 0}
-            //   onChange={(event, expanded) => {
-            //     setIndex(expanded ? 0 : null);
-            //   }}
-            >
-              <div className="grayPositionContainer bgPositionCard">
-                {/* the part that appear when card is collapsed */}
-                <AccordionSummary>
-                  <h2 className="headOfRightDiv md:w-4/5">
-                    {e.title.toUpperCase()}
-                  </h2>
-                  <div className="txtAlone">
-                    <p className="ms:visible sm:hidden">
-                     {e.description}
-                    </p>
-                  </div>
-                </AccordionSummary>
-                {/* the part that appear when the card is expanded */}
 
-                <AccordionDetails className="accordionDetails">
-                  <div
-                    className="everyPosition md:flex"
-                    style={{ borderRadius: "0.8rem" }}
-                  >
-                    <div className="md:w-1/2 ">
-                      <div className="w-4/5 mx-auto flex flex-col gap-7 sm:pb-10 bottomBorderInSm">
-                        <div className="txtAlone">
-                          <span>
-                            We're seeking a {e.title} to join our
-                            team and contribute to the development of our
-                            cutting-edge products and services.
-                          </span>
-                        </div>
-                        {/* position responsibilities */}
-                        <div className="headAndUl flex flex-col md:gap-4 sm:gap-3">
-                          <h3>Responsibilities</h3>
-                          
-                          <ul className="positionsList ">
-                            {e.responsibilities?.split('\n').map(line => line.trim()).map((e,i)=>(
-                                <li key={i}>{e}</li>
-                            ))}
-                          </ul>
-                        </div>
-                        {/* position benefits */}
-                        <div className="headAndUl flex flex-col md:gap-4 sm:gap-3">
-                          <h3>Benefits</h3>
-                          <ul className="positionsList">
-                          {e.benefits?.split('\n').map(line => line.trim()).map((e,i)=>(
-                                <li key={i}>{e}</li>
-                            ))}
-                          </ul>
-                        </div>
-                        {/* position requirements */}
-                        <div className="headAndUl flex flex-col md:gap-4 sm:gap-3">
-                          <h3 className="movingH3">Requirement</h3>
-                          <div>
-                            <ul className="positionsList">
-                            {e.requirement?.split('\n').map(line => line.trim()).map((e,i)=>(
-                                <li key={i}>{e}</li>
-                            ))}
+          {vacanciesList.map((e, i) => (
+            <div
+              key={i}
+              className="grayPositionBorder carrerPositionCollapse md:py-0 sm:py-4"
+            >
+              <Accordion
+                defaultExpanded={i == 0 ? true : false}
+                sx={{
+                  "&.Mui-expanded": {
+                    border: "1px solid #53ACFF",
+                    borderRadius: "0.5rem",
+                  },
+                  "&.Mui-expanded p": {
+                    display: "none",
+                  },
+                  "&.Mui-expanded h2": {
+                    color: "#53ACFF",
+                  },
+                  padding: "0 2rem",
+                }}
+                //   expanded={i === 0}
+                //   onChange={(event, expanded) => {
+                //     setIndex(expanded ? 0 : null);
+                //   }}
+              >
+                <div className="grayPositionContainer bgPositionCard">
+                  {/* the part that appear when card is collapsed */}
+                  <AccordionSummary>
+                    <h2 className="headOfRightDiv md:w-4/5">
+                      {e.title.toUpperCase()}
+                    </h2>
+                    <div className="txtAlone">
+                      <p className="ms:visible sm:hidden">{e.description}</p>
+                    </div>
+                  </AccordionSummary>
+                  {/* the part that appear when the card is expanded */}
+
+                  <AccordionDetails className="accordionDetails">
+                    <div
+                      className="everyPosition md:flex"
+                      style={{ borderRadius: "0.8rem" }}
+                    >
+                      <div className="md:w-1/2 ">
+                        <div className="w-4/5 mx-auto flex flex-col gap-7 sm:pb-10 bottomBorderInSm">
+                          <div className="txtAlone">
+                            <span>
+                              We're seeking a {e.title} to join our team and
+                              contribute to the development of our cutting-edge
+                              products and services.
+                            </span>
+                          </div>
+                          {/* position responsibilities */}
+                          <div className="headAndUl flex flex-col md:gap-4 sm:gap-3">
+                            <h3>Responsibilities</h3>
+
+                            <ul className="positionsList ">
+                              {e.responsibilities
+                                ?.split("\n")
+                                .map((line) => line.trim())
+                                .map((e, i) => (
+                                  <li key={i}>{e}</li>
+                                ))}
                             </ul>
                           </div>
-                        </div>
-                      </div>
-                    </div>
-                    {/* form to be filled to complete user application for position */}
-                    <div className="md:w-1/2 sm:pt-10">
-                      <div className="w-4/5 positionRightPart flex flex-col md:gap-20 sm:gap-7 sm:mx-auto sm:pb-4">
-                        <div className="flex flex-col gap-4">
-                          <h3>Fill Application</h3>
-                          <LabelAndInput
-                            labeltext="First Name"
-                            required={true}
-                            placeholder="Enter your name"
-                          />
-                          <LabelAndInput
-                            labeltext="Last Name"
-                            required={true}
-                            placeholder="Enter your name"
-                          />
-                          <LabelAndInput
-                            labeltext="Email"
-                            required={true}
-                            placeholder="Email"
-                          />
-                          <LabelAndInput
-                            labeltext="Portfolio Link"
-                            required={false}
-                          />
-                          <LabelAndInput
-                            labeltext="Get Hub Link"
-                            required={false}
-                          />
-
-                          <FormLabel
-                            required
-                            className="mb-1 labelfont"
-                            sx={{
-                              "& .MuiFormLabel-asterisk": {
-                                color: "white",
-                              },
-                            }}
-                          >
-                            Upload CV
-                          </FormLabel>
-                          <div className="selectFile">
-                            <input type="file" id="files" className="hidden" />
-                            <label htmlFor="files" className="w-fit fileBtn">
-                              Attach pdf
-                            </label>
+                          {/* position benefits */}
+                          <div className="headAndUl flex flex-col md:gap-4 sm:gap-3">
+                            <h3>Benefits</h3>
+                            <ul className="positionsList">
+                              {e.benefits
+                                ?.split("\n")
+                                .map((line) => line.trim())
+                                .map((e, i) => (
+                                  <li key={i}>{e}</li>
+                                ))}
+                            </ul>
+                          </div>
+                          {/* position requirements */}
+                          <div className="headAndUl flex flex-col md:gap-4 sm:gap-3">
+                            <h3 className="movingH3">Requirement</h3>
+                            <div>
+                              <ul className="positionsList">
+                                {e.requirement
+                                  ?.split("\n")
+                                  .map((line) => line.trim())
+                                  .map((e, i) => (
+                                    <li key={i}>{e}</li>
+                                  ))}
+                              </ul>
+                            </div>
                           </div>
                         </div>
+                      </div>
+                      {/* form to be filled to complete user application for position */}
+                      <div className="md:w-1/2 sm:pt-10">
+                        <div className="w-4/5 positionRightPart flex flex-col md:gap-20 sm:gap-7 sm:mx-auto sm:pb-4">
+                          <div className="flex flex-col gap-4">
+                            <h3>Fill Application</h3>
+                            <LabelAndInput
+                              labeltext="First Name"
+                              required={true}
+                              placeholder="Enter your name"
+                              onChangeHandler={(e) =>
+                                setFirstName(e.target.value)
+                              }
+                            />
+                            <LabelAndInput
+                              labeltext="Last Name"
+                              required={true}
+                              placeholder="Enter your name"
+                              onChangeHandler={(e) =>
+                                setLastName(e.target.value)
+                              }
+                            />
+                            <LabelAndInput
+                              labeltext="Email"
+                              required={true}
+                              placeholder="Email"
+                              onChangeHandler={(e) => setEmail(e.target.value)}
+                            />
+                            <LabelAndInput
+                              labeltext="Portfolio Link"
+                              required={false}
+                              onChangeHandler={(e) =>
+                                setPortfolioLink(e.target.value)
+                              }
+                            />
+                            <LabelAndInput
+                              labeltext="Get Hub Link"
+                              required={false}
+                              onChangeHandler={(e) =>
+                                setGitHubLink(e.target.value)
+                              }
+                            />
 
-                        <div className="mx-auto">
-                          <Button className="changesBtn applyBtn w-fit ">
-                            Apply
-                          </Button>
+                            <FormLabel
+                              required
+                              className="mb-1 labelfont"
+                              sx={{
+                                "& .MuiFormLabel-asterisk": {
+                                  color: "white",
+                                },
+                              }}
+                            >
+                              Upload CV
+                            </FormLabel>
+                            <div className="selectFile">
+                              <input
+                                type="file"
+                                id="files"
+                                className="hidden"
+                                onChange={(e) => setCv(e.target.files[0])}
+                              />
+
+                              <label htmlFor="files" className="w-fit fileBtn">
+                                Attach pdf
+                              </label>
+                            </div>
+                          </div>
+
+                          <div className="mx-auto">
+                            <Button className="changesBtn applyBtn w-fit ">
+                              Apply
+                            </Button>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                </AccordionDetails>
-              </div>
-            </Accordion>
-          </div>
+                  </AccordionDetails>
+                </div>
+              </Accordion>
+            </div>
           ))}
-          
         </AccordionGroup>
       </div>
     </>
